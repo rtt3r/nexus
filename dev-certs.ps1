7e33a313790c17088ca23f7559aa8f6f15bd2934
Anderson Ritter de Souza <anderdsouza@gmail.com>
2025-03-27 07:52:31 -0300

    Ajustes docker

--- dev-certs.ps1
+++ dev-certs.ps1
@@ -1,3 +1,6 @@
+mkdir $Env:USERPROFILE/.aspnet/https
+
 dotnet dev-certs https --clean
-dotnet dev-certs https -ep $Env:APPDATA/ASP.NET/Https/Nexus.Core.Web.pfx
+dotnet dev-certs https -ep $Env:USERPROFILE/.aspnet/https/Development.pfx -p c1bc6816-f70f-42e3-a71f-4ab75a294755
 dotnet dev-certs https --trust
+dotnet dev-certs https --check
